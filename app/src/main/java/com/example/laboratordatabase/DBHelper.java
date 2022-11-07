24d1874794c0f99d96c04b232d4fdad6b0a331e8
SergiuA17 <98698995+SergiuLA17@users.noreply.github.com>
2022-11-07 14:34:48 +0200

    add

---
+++ app/src/main/java/com/example/laboratordatabase/DBHelper.java
@@ -0,0 +1,40 @@
+package com.example.laboratordatabase;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+class DBHelper extends SQLiteOpenHelper {
+    final String LOG_TAG = "myLogs";
+    String DBname;
+    SQLiteDatabase db;
+
+    public DBHelper(Context context, String dbname) {
+        super(context, dbname, null, 1);
+        this.DBname = dbname;
+    }
+
+    public void createT(SQLiteDatabase db, String tableN, String[] FieldsN,
+                        String[] FieldsT, int numberOfFields) {
+        String ss = "create table " + tableN + "(";
+        for (int i = 0; i < numberOfFields - 1; i++) {
+            ss = ss + FieldsN[i] + "  " + FieldsT[i] + ", ";
+        }
+        ss = ss + FieldsN[numberOfFields - 1] + "  " + FieldsT[numberOfFields - 1];
+        ss = ss + ");";
+        Log.d("Create Table:", tableN + "  :  " + ss);
+        db.execSQL(ss);
+        Log.d("a fost creat", "tabelul" + tableN + "  ," + ss);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        Log.d(LOG_TAG, "--- onCreate database ---");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+
+    }
+}
